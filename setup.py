4bcd73242bce5da65345c96c143f384bbc44f641
rj-agilepm <rj@agilepm.cloud>
2018-01-04 19:12:06 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='poibin',
+   version='0.1',
+   description='Poisson Binomial',
+   packages=['poibin'],
+   install_requires=['pytest', 'numpy'],
+)
